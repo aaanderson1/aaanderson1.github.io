15adecbaabc03f1e5618b8326213d88ffc68038d
Allison Anderson <andersonaallison@gmail.com>
2021-08-19 20:25:32 -0700

    update

--- connect-four/index.js
+++ connect-four/index.js
@@ -1,6 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => 
 {
 const squares = document.querySelectorAll('.grid div')
+const result = document.querySelector('#result')
+const displayCurrentPlayer = document.querySelector('#current-player')
+let CurrentPlayer = 1
 
+for (let i = 0; i < squares.length; i++) {
+    squares[i].onClick = () => {
+//if the square below your current square is taken, you can go on top of it
+
+}
+}
 
 })
